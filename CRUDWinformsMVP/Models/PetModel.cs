477bb9540cbc17e7f5c55ebe2a1184b6445e7f8f
qindavid <qindavid@microsoft.com>
2022-11-09 19:26:16 -0800

    Initial commit

---
+++ CRUDWinformsMVP/Models/PetModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WinFormsApp1.Models
+{
+    public class PetModel
+    {
+        private int id;
+        private string? name;
+        private string? type;
+        private string? color;
+
+        [DisplayName("Pet ID")]
+        public int Id { get => id; set => id = value; }
+
+        [DisplayName("Pet Name")]
+        [Required(ErrorMessage = "The pet name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The pet name must be between 2 and 50 characters")]
+        public string? Name 
+        { 
+            get => name;
+            set => name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        [DisplayName("Pet Type")]
+        [Required(ErrorMessage = "The pet type is required")]
+        public string? Type 
+        { 
+            get => type; 
+            set => type = value ?? throw new ArgumentNullException(nameof(value)); 
+        }
+
+        [DisplayName("Pet Color")]
+        [Required(ErrorMessage = "The pet color is required")]
+        public string? Color 
+        {
+            get => color; 
+            set => color = value ?? throw new ArgumentNullException(nameof(value)); 
+        }
+    }
+}
